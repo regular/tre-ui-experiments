cc8834516fd19915f258ebe8ddf4c373ba6d9c3c
Jan Bölsche <regular.gonzales@googlemail.com>
2018-09-07 19:50:15 +0200

    Assign new sort-index based on new neighbours sort indices

--- sort.js
+++ sort.js
@@ -7,14 +7,21 @@ const watch = require('mutant/watch')
 const computed = require('mutant/computed')
 
 const items = [
-  Value({id: 0, a:30, n:'a'}),
-  Value({id: 1, a:10, n:'c'}),
-  Value({id: 2, a:20, n:'b'})
+  Value({id: '0', a:30, n:'a'}),
+  Value({id: '1', a:10, n:'c'}),
+  Value({id: '2', a:20, n:'b'})
 ]
 
 const byName = (a,b) => a.n == b.n ? 0 : a.n > b.n ? 1 : -1
 const byNumber = (a,b) => a.a == b.a ? 0 : a.a > b.a ? 1 : -1
 const criteria = Value(byNumber)
+
+const mutantArray = MutantArray(items)
+const sortedArray = computed([mutantArray, criteria], (a, c) => {
+  console.log(`sort ${JSON.stringify(a)}`)
+  return a.sort(c)
+})
+const sortedElements = MutantMap(sortedArray, render, {comparer})
 
 function comparer(a, b) {
   if (a === b) return true
@@ -53,25 +60,35 @@ function render(x) {
         e.stopPropagation()
         const dropped_id = e.dataTransfer.getData('text/plain')
         const where = e.target.classList.contains('above') ? 'above' : 'below'
-        if (`${dropped_id}` == `${x.id}`) {
-          console.log(`dropped ${x.id} onto itself.`)
+        if (dropped_id == x.id) {
+          console.log(`dropped ${dropped_id} onto itself.`)
           return false
         }
-        sortedArray.find( o=>String(o.id)==dropped_id)
-        console.log(`dropped ${dropped_id} ${where} ${x.id}`)
+        const arr = sortedArray()
+        const our_idx = arr.indexOf(
+          arr.find(o=>o.id == x.id)
+        )
+        const other_idx = our_idx + (where == 'above' ? -1 : +1)
+        const indices = [our_idx, other_idx].sort()
+        if (indices.map(i=>(arr[i] || {id:''}).id).includes(dropped_id)) {
+          console.log(`dropped ${dropped_id} onto itself.`)
+          return false
+        }
+        const lower = indices[0] >= 0 ? arr[indices[0]].a : arr[0].a - 10
+        const upper = indices[1] < arr.length ? arr[indices[1]].a : arr[arr.length-1].a + 10
+        console.log(`dropped ${dropped_id} ${where} ${x.id}, between index ${indices[0]} and ${indices[1]}, sort-index between ${lower} and ${upper}`)
+        
+        const droppedObv = mutantArray.find(o=>{
+          return o().id == dropped_id
+        })
+        if (!droppedObv) throw new Error('dropped_id not found in mutantArray')
+        droppedObv.set(Object.assign({}, droppedObv(), {a: (upper + lower) / 2})) 
         return false
       }
     },
     `#${x.id} / ${x.a}: ${x.n}`
   )
 }
-
-const arr = MutantArray(items)
-const sortedArray = computed([arr, criteria], (a, c) => {
-  console.log(`sort ${JSON.stringify(a)}`)
-  return a.sort(c)
-})
-const sortedElements = MutantMap(sortedArray, render, {comparer})
 
 function makeSortButton(label, sorter) {
   return h(
@@ -112,6 +129,20 @@ function makeSortDropDown(sorters) {
 }
 
 document.body.appendChild(
+  h('div', [
+    makeSortDropDown([
+      {label: 'by Name',   sortf: byName},
+      {label: 'by Number', sortf: byNumber}
+    ]),
+    h('.container', sortedElements)
+  ])
+)
+
+mutantArray.push(Value({id: 'new', a:15, n:'foo'})) // calls render
+items[2].set({id:'2', a:80, n:'d'}) 
+criteria.set(byName)
+
+document.body.appendChild(
   h('style', `
     .active {
       background: lightblue;
@@ -140,16 +171,3 @@ document.body.appendChild(
   `)
 )
 
-document.body.appendChild(
-  h('div', [
-    makeSortDropDown([
-      {label: 'by Name',   sortf: byName},
-      {label: 'by Number', sortf: byNumber}
-    ]),
-    h('.container', sortedElements)
-  ])
-)
-
-arr.push({id: 'new', a:15, n:'foo'}) // calls render
-items[2].set({id:2, a:80, n:'d'}) 
-criteria.set(byName)
